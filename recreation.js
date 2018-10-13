12ebb5082bc0c890bf9111b6d27e492a990f569e
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-10-13 02:26:25 +0000

    re-creation incomplete. Need to run the codes

---
+++ recreation.js
@@ -0,0 +1,18 @@
+var practice = "I am re-creating the project from yesterday";
+//a variable
+
+var farmAnimals = ["cow", "pig", "chicken", "duck", "lamb", "goat"];
+var evenSteven = [20, 40, 60, 80]
+//arrays can contain 'strings' like line 4, or integers like line 5. Remember that the first item in an array is always number zero
+
+var face {
+    eyes: "blue",
+    ears: 2,
+    lips: "red",
+    nose: "crooked",
+    skin: "glowing"
+};
+
+//objects can contain words (strings) or integers. Take note that colons are used, not '=' for objects
+
+//enter 'node' to the command line to enter REPL
