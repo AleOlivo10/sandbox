6195e012c9d1195442e4650c7f0943e15bd00180
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-10-13 15:09:40 +0000

    adding one more file

--- recreation.js
+++ recreation.js
@@ -2,10 +2,10 @@ var practice = "I am re-creating the project from yesterday";
 //a variable
 
 var farmAnimals = ["cow", "pig", "chicken", "duck", "lamb", "goat"];
-var evenSteven = [20, 40, 60, 80]
+var evenSteven = [20, 40, 60, 80];
 //arrays can contain 'strings' like line 4, or integers like line 5. Remember that the first item in an array is always number zero
 
-var face {
+var face = {
     eyes: "blue",
     ears: 2,
     lips: "red",
@@ -13,6 +13,12 @@ var face {
     skin: "glowing"
 };
 
+var obvious = {
+    one: 1,
+    two: 2,
+    three: 3,
+};
+}
+
 //objects can contain words (strings) or integers. Take note that colons are used, not '=' for objects
 
-//enter 'node' to the command line to enter REPL
---
+++ ref-notes.js
@@ -0,0 +1,11 @@
+//write 'node' to enter REPL line to run individual code
+
+//copy+paste entire variables onto repl line so that it knows what code to run. for example: write 'var example = "this is an example"' onto the line 
+//Then, you write 'example' onto the line and it will give you "This is an example"
+
+//same with array, but you can put a number inside of square brackets to determine which item in the array you are choosing
+
+//for an object you write objectName.whatever and it gives you another thing.
+//if eyes: 2, writing objectName.eyes, it will give you 2
+
+because everything is notes, is this the only visible line?
