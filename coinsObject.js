0528688257e4cd967dac24b6ab045bab96bd8798
Alejandra Olivo <alejandraolivo.student@careerdevs.com>
2018-10-13 01:11:05 +0000

    Mess around in this folder

---
+++ coinsObject.js
@@ -0,0 +1,15 @@
+var assignment = "Create an object for coins";
+//an example of a variable
+
+var currencyAbbrv = ["AUD", "GBP", "EUR", "JPY", "CHF", "USD", "AFN", "ALL", "DZD", "AOA"];
+//an example of an array
+
+var USD = {
+penny: 0.01,
+nickel: 0.05,
+dime: 0.10,
+halfDollar: 0.50,
+quarter: 0.25
+};
+//an example of an object
+
